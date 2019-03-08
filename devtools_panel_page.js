3bb102307ea232182ebef51dd301aa78d27b6a51
Nicholas Jamieson <nicholas.jamieson@rexsoftware.com.au>
2019-03-08 12:18:29 +1000

    chore: Add received messages to DOM.

--- devtools_panel_page.js
+++ devtools_panel_page.js
@@ -3,9 +3,15 @@
 const backgroundConnection = chrome.runtime.connect({
     name: `panel@${chrome.devtools.inspectedWindow.tabId}`
 });
-backgroundConnection.onMessage.addListener(message => console.log("received", message));
+backgroundConnection.onMessage.addListener(message => {
+    console.log("received", message);
+    document.querySelector("#received").innerHTML = JSON.stringify(message);
+});
 document.querySelector("#post").addEventListener("click", () => {
-    const message = { data: "from panel" };
+    const message = {
+        data: "from panel",
+        timestamp: Date.now()
+    };
     console.log("posting", message);
     backgroundConnection.postMessage(message);
 });
