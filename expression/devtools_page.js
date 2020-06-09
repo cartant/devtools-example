f2d61b49c5a3199b2a48ec887d891ce45a375f96
Nicholas Jamieson <nicholas@cartant.com>
2020-06-10 08:52:02 +1000

    chore: Add comments.

--- expression/devtools_page.js
+++ expression/devtools_page.js
@@ -1,5 +1,17 @@
+// Create another pane that will be shown on the sidebar within the 'Elements'
+// DevTools tab.
+
 chrome.devtools.panels.elements.createSidebarPane("Example", (sidebar) => {
-  const update = () => {
+  // Use setExpression to evaluate an object that contains the client position
+  // for the selected element: `$0`
+  //
+  // The expression is evaluated in the JavaScript context of the inspected
+  // page, so any globals in that context could be used within the expression.
+  //
+  // Object.setPrototypeOf is used here to prevent the Object prototype being
+  // shown in the pane content.
+
+  function update() {
     sidebar.setExpression(
       `Object.setPrototypeOf({
         left: $0.clientLeft,
@@ -9,7 +21,11 @@ chrome.devtools.panels.elements.createSidebarPane("Example", (sidebar) => {
       }, null)`,
       `Client position`
     );
-  };
+  }
   update();
+
+  // Whenever the selection in the selected element changes, update the
+  // expression so that the pane content is updated.
+
   chrome.devtools.panels.elements.onSelectionChanged.addListener(update);
 });
