b7091f3aa0ecf4f540a53e7aa968a567acaffd6f
Nicholas Jamieson <nicholas@cartant.com>
2020-06-10 16:35:45 +1000

    chore: Add placeholder text.

--- eval/content_script.js
+++ eval/content_script.js
@@ -1,11 +1,11 @@
 "use strict";
 
-// A naïve implementation of a function that returns the 'best' testing-library
-// query for the specified element.
+// A placeholder implementation of a function that returns the 'best'
+// testing-library query for the specified element.
 
 function suggestQuery(element) {
   return {
     element,
-    query: "whatever",
+    query: "suggestion goes here",
   };
 }
